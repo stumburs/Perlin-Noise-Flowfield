5ef262167dd4a1db4da80a6837002b030bfa4455
stumburs <stumburs@inbox.lv>
2022-09-23 12:03:39 +0300

    Drawing basic perlin noise.

--- Perlin Noise Flowfield/Perlin Noise Flowfield.cpp
+++ Perlin Noise Flowfield/Perlin Noise Flowfield.cpp
@@ -1,20 +1,48 @@
-#include <iostream>
 #include "raylib.h"
 #include "PerlinNoise.h"
+#include "Functions.h"
+#include <vector>
+#include <array>
+
+
+std::array<std::array<double, 720>, 1280> flowfield;
 
 int main()
 {
     InitWindow(1280, 720, "Flowfield");
 	SetTargetFPS(60);
 
+	// Perlin noise setup
+	const siv::PerlinNoise::seed_type seed = 123456u;
+	const siv::PerlinNoise perlin{ seed };
+
 	while (!WindowShouldClose())
 	{
 		// Update
+		{
+
+			for (int x = 0; x < 1280; x++)
+			{
+				for (int y = 0; y < 720; y++)
+				{
+					flowfield[x][y] = (Map(perlin.octave2D_01((x * 0.01), (y * 0.01), 4), 0, 1, 0, 255));
+				}
+			}
+			
+		}
 
 		// Draw
 		BeginDrawing();
 		{
 			ClearBackground(BLACK);
+
+			for (int x = 0; x < 1280; x++)
+			{
+				for (int y = 0; y < 720; y++)
+				{
+					DrawPixel(x, y, Color(flowfield[x][y], 255, 255, 255));
+				}
+			}
 		}
 		EndDrawing();
 	}
